fe55edb3abb556d8198b80afa33ff3e32a97b601
Tao <tao.li06@sap.com>
2024-12-03 12:08:47 +0000

    Create docker file and github flow files

--- src/include/user_app/nat64_conf_opts.h
+++ src/include/user_app/nat64_conf_opts.h
@@ -5,9 +5,12 @@
 #include <getopt.h>
 #include <unistd.h>
 
+#define NAT64_CONF_DEFAULT_CONF_FILE "/etc/nat64_config.conf"
+
 void nat64_print_usage(const char *prgname);
 
 int nat64_parse_args(int argc, char **argv);
+int nat64_parse_config_file(const char *env_filename);
 
 const char *nat64_get_addr_port_pool_str(void);
 const char *nat64_get_attach_south_iface_str(void);
