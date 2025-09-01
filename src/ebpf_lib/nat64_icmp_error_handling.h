021a7d7f63613d4045b95b9510eda57369771752
Tao <tao.li06@sap.com>
2025-09-01 14:15:47 +0000

    Add one missing reuse statement

--- src/ebpf_lib/nat64_icmp_error_handling.h
+++ src/ebpf_lib/nat64_icmp_error_handling.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: Apache-2.0
+
+
 #ifndef _NAT64_ICMP_ERROR_HANDLING_H_
 #define _NAT64_ICMP_ERROR_HANDLING_H_
 
